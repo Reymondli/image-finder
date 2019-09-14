033c57e6d696d4a9f4e33a4b20213a4926385d21
zimingli <reymondli0510@gmail.com>
2019-09-14 17:26:01 -0400

    Hide unsplash token

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// Assign unsplash token here.
+const unsplashToken = "";
+
 export default axios.create({
   headers: {
-    Authorization:
-      "Client-ID 3fdc7276ff3fa833b0f2d0fce797975e3d741728db02ad64d15ff512382a0981"
+    Authorization: `Client-ID ${unsplashToken}`
   },
   baseURL: "https://api.unsplash.com"
 });
